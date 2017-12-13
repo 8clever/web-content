733c864a982de2feba073ba8341d61adbdc9080b
rustedhorse <dmitruy1997@mail.ru>
2017-12-13 17:56:15 +0100

    test

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/add_project', function(req, res, next) {
+  res.render( 'add_project', { title: 'Add project' });
+});
+
 module.exports = router;
